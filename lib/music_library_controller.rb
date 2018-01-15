6796966e8747281e5f8eea613609d84a22654452
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-15 19:37:28 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,7 @@ class MusicLibraryController
   end
 
   def list_genres
-    binding.pry
+
     genre_alpha = Genre.all.sort {|a, b| a.name <=> b.name}
     genre_alpha.map.with_index do |genre, index|
       puts "#{index + 1}. #{genre.name}"
